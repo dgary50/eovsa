f0a7b1262ee541fdafe9f3de37e8ca3ba3e88bd3
user <user@pipeline.solar.pvt>
2017-05-17 16:49:53 +0000

    updated the plotting functions of refcal_anal.py

--- refcal_anal.py
+++ refcal_anal.py
@@ -18,7 +18,11 @@ bl2ord = p.bl_list()
 
 
 def findfiles(trange, projid='PHASECAL', srcid=None):
-    '''identify refcal files'''
+    '''identify refcal files
+    ***Optional Keywords***
+    projid: String--PROJECTID in UFBD records. Default is PHASECAL
+    srcid: String--SOURCEID in UFBD records. Can be a string or a list
+    '''
     from util import nearest_val_idx
     import struct, time, glob, sys, socket
     import dump_tsys
@@ -41,7 +45,13 @@ def findfiles(trange, projid='PHASECAL', srcid=None):
         ufdb = dump_tsys.rd_ufdb(trange[0])
 
     if srcid:
-        scanidx, = np.where((ufdb['PROJECTID'] == projid) & (ufdb['SOURCEID'] == srcid))
+        if type(srcid) is str:
+            srcid=[srcid]
+        sidx_=np.array([])
+        for sid in srcid:
+            sidx, = np.where((ufdb['PROJECTID'] == projid) & (ufdb['SOURCEID']  == sid))
+            sidx_=np.append(sidx_,sidx)
+        scanidx=np.sort(sidx_).astype('int')
     else:
         scanidx, = np.where(ufdb['PROJECTID'] == projid)
     # List of scan start times
@@ -73,7 +83,7 @@ def findfiles(trange, projid='PHASECAL', srcid=None):
     return {'scanlist': flist, 'srclist': srclist, 'tstlist': tstlist, 'tedlist': tedlist}
 
 
-def rd_refcal(trange, projid='PHASECAL', srcid=None, quackint=180.):
+def rd_refcal(trange, projid='PHASECAL', srcid=None, quackint=180.,navg=3):
     '''take a time range from the Time object, e.g., trange=Time(['2017-04-08T05:00','2017-04-08T15:30']),
        a projectid, and source id, return visibility data for all baselines correlated with ant 14.
        **Optional keywords***
@@ -94,7 +104,7 @@ def rd_refcal(trange, projid='PHASECAL', srcid=None, quackint=180.):
     bandnames = []
     times = []
     for n, scan in enumerate(scanlist):
-        out = ri.read_idb([scan], navg=3, quackint=quackint)
+        out = ri.read_idb([scan], navg=navg, quackint=quackint)
         times.append(out['time'])
         nt = len(out['time'])
         if nt == 0:
@@ -114,12 +124,13 @@ def rd_refcal(trange, projid='PHASECAL', srcid=None, quackint=180.):
             'vis': vis, 'bandnames': bandnames, 'times': times}
 
 
-def graph(out, visavg=None, ant_str='ant1-13', bandplt=[5, 11, 17, 23], scanidx=None, pol=0):
+def graph(out, refcal=None, ant_str='ant1-13', bandplt=[5, 11, 17, 23], scanidx=None, pol=0,
+          tformat='%H:%M'):
     '''Produce a figure showing phases and amplitudes of selected antennas, bands, and polarization.
-       Optionally takes in a time averaged value 'visavg' to show the selected time range and plot the
-       averaged phase and amplitudes'''
+       Optionally takes in time averaged 'refcal' (can be from refcal_anal() or sql database) 
+       to show the selected time range and plot the averaged phase and amplitudes'''
     # takes input from rd_refcal (out)
-    date_format = mdates.DateFormatter('%H:%M')
+    date_format = mdates.DateFormatter(tformat)
     # make a color plot showing the phase
     # ri.summary_plot_pcal(out)
     if scanidx:
@@ -146,41 +157,54 @@ def graph(out, visavg=None, ant_str='ant1-13', bandplt=[5, 11, 17, 23], scanidx=
     f2, ax2 = plt.subplots(nant, nband, figsize=(12, 8))
     for n, scan in enumerate(scanlist):
         ts = Time(times[n], format='jd').plot_date
-
+        try:
         # make plots of phase and amp vs. time on all 34 bands
+            for ant in ant_list:
+                for b, bd in enumerate(bds0):
+                    ph_deg = np.angle(vis[n][ant, pol, bd - 1], deg=True)
+                    amp = np.abs(vis[n][ant, pol, bd - 1])
+                    ax1[ant, b].plot_date(ts, ph_deg, '.', markersize=5)
+                    ax1[ant, b].xaxis.set_major_formatter(date_format)
+                    ax2[ant, b].plot_date(ts, amp, '.', markersize=5)
+                    ax2[ant, b].xaxis.set_major_formatter(date_format)
+                    ax1[ant, b].set_ylim([-180., 180.])
+                    if ant == 0:
+                        ax1[ant, b].set_title('Band ' + str(bd))
+                        ax2[ant, b].set_title('Band ' + str(bd))
+                    if b == 0:
+                        ax1[ant, b].text(-0.4, 0.5, 'Ant ' + str(ant + 1), ha='center', va='center',
+                                         transform=ax1[ant, b].transAxes, fontsize=10)
+                        ax2[ant, b].text(-0.4, 0.5, 'Ant ' + str(ant + 1), ha='center', va='center',
+                                         transform=ax2[ant, b].transAxes, fontsize=10)
+                    else:
+                        ax1[ant, b].set_yticks([])
+                        ax2[ant, b].set_yticks([])
+        except:
+            print 'Failure in plotting scan: ', scan
+            continue
+    if refcal:
         for ant in ant_list:
             for b, bd in enumerate(bds0):
-                ph_deg = np.angle(vis[n][ant, pol, bd - 1], deg=True)
-                amp = np.abs(vis[n][ant, pol, bd - 1])
-                ax1[ant, b].plot_date(ts, ph_deg, '.', markersize=5)
-                ax1[ant, b].xaxis.set_major_formatter(date_format)
-                ax2[ant, b].plot_date(ts, amp, '.', markersize=5)
-                ax2[ant, b].xaxis.set_major_formatter(date_format)
-                if visavg:
-                    phavg = np.angle(visavg['vis'][ant, pol, bd - 1], deg=True)
-                    ampavg = np.abs(visavg['vis'][ant, pol, bd - 1])
-                    flg = visavg['flag'][ant, pol, bd - 1]
-                    if flg == 0:
-                        bt = Time(visavg['time'][0], format='jd').plot_date
-                        et = Time(visavg['time'][-1], format='jd').plot_date
-                        ax1[ant, b].plot_date([bt, et], [phavg, phavg], '-r')
-                        ax1[ant, b].plot_date([bt, bt], [-200, 200], '--k')
-                        ax1[ant, b].plot_date([et, et], [-200, 200], '--k')
-                        ax2[ant, b].plot_date([bt, et], [ampavg, ampavg], '-r')
-                        ax2[ant, b].plot_date([bt, bt], [0, np.max(amp)], '--k')
-                        ax2[ant, b].plot_date([et, et], [0, np.max(amp)], '--k')
-                ax1[ant, b].set_ylim([-180., 180.])
-                if ant == 0:
-                    ax1[ant, b].set_title('Band ' + str(bd))
-                    ax2[ant, b].set_title('Band ' + str(bd))
-                if b == 0:
-                    ax1[ant, b].text(-0.4, 0.5, 'Ant ' + str(ant + 1), ha='center', va='center',
-                                     transform=ax1[ant, b].transAxes, fontsize=10)
-                    ax2[ant, b].text(-0.4, 0.5, 'Ant ' + str(ant + 1), ha='center', va='center',
-                                     transform=ax2[ant, b].transAxes, fontsize=10)
-                else:
-                    ax1[ant, b].set_yticks([])
-                    ax2[ant, b].set_yticks([])
+                phavg = np.degrees(refcal['pha'][ant, pol, bd - 1])
+                ampavg = refcal['amp'][ant, pol, bd - 1]
+                flg = refcal['flag'][ant, pol, bd - 1]
+                if flg == 0:
+                    if 't_bg'  in refcal.keys():
+                        bt = Time(refcal['t_bg'][0], format='jd').plot_date
+                    else:
+                        bt = Time(times[0][0], format='jd').plot_date
+                    if 't_ed' in refcal.keys():
+                        et = Time(refcal['t_ed'][-1], format='jd').plot_date
+                    else:
+                        et = Time(times[-1][-1], format='jd').plot_date
+                    ax1[ant, b].plot_date([bt, et], [phavg, phavg], '-r')
+                    ax1[ant, b].plot_date([bt, bt], [-200, 200], '--k')
+                    ax1[ant, b].plot_date([et, et], [-200, 200], '--k')
+                    ax2[ant, b].plot_date([bt, et], [ampavg, ampavg], '-r')
+                    ax2[ant, b].plot_date([bt, bt], [0, np.max(amp)], '--k')
+                    ax2[ant, b].plot_date([et, et], [0, np.max(amp)], '--k')
+                    ax1[ant, b].plot_date(refcal['timestamp'].plot_date, phavg, 'o')
+                    ax2[ant, b].plot_date(refcal['timestamp'].plot_date, ampavg, 'o')
 
 def refcal_anal(out, timerange=None, scanidx=None, minsnr=0.7, bandplt=[5, 11, 17, 23]):
     '''Analyze the visibility data from rd_refcal and return time averaged visibility values and flags.
@@ -250,7 +274,11 @@ def refcal_anal(out, timerange=None, scanidx=None, minsnr=0.7, bandplt=[5, 11, 1
     # flag zero values (e.g., antennas or bands not observed)
     zeroind = np.where(np.abs(vis_ == 0))
     flag[zeroind] = 1
-    visavg = {'pha': np.angle(vis_), 'amp': np.abs(vis_), 'vis': vis_, 'time': timeavg, 'flag': flag}
+    # timestamps
+    timestamp = Time(np.mean(timeavg), format='jd')
+    timestamp_gcal = Time((tstlist[0].jd + tedlist[0].jd) / 2., format='jd')
+    visavg = {'pha': np.angle(vis_), 'amp': np.abs(vis_), 'timestamp': timestamp, 
+              't_bg':timeavg[0], 't_ed':timeavg[-1], 'flag': flag}
     graph(out, visavg, scanidx=scanidx, bandplt=bandplt)
     f2, ax2 = plt.subplots(2, 13, figsize=(12, 5))
     f3, ax3 = plt.subplots(2, 13, figsize=(12, 5))
@@ -279,11 +307,46 @@ def refcal_anal(out, timerange=None, scanidx=None, minsnr=0.7, bandplt=[5, 11, 1
                 ax3[pol, ant].set_title('Ant ' + str(ant + 1))
                 ax3[pol, ant].set_xticks([])
     refcal = vis_
-    timestamp = Time(np.mean(timeavg), format='jd')
-    timestamp_gcal = Time((tstlist[0].jd + tedlist[0].jd) / 2., format='jd')
     return {'refcal': refcal, 'flag': flag, 't_mid': timestamp, 't_gcal': timestamp_gcal,
-            't_bg': Time(timeavg[0], format='jd'), 't_end': Time(timeavg[-1], format='jd')}
+            't_bg': Time(timeavg[0], format='jd'), 't_ed': Time(timeavg[-1], format='jd')}
 
+def graph_results(refcal,unwrap=True):
+    '''Provide an output from sql2refcal() (single refcal result) or sql2refcalX() (list of refcal results).
+       Plot phase and amp vs. bands'''
+    f1, ax1 = plt.subplots(2, 13, figsize=(12, 5))
+    f2, ax2 = plt.subplots(2, 13, figsize=(12, 5))
+    allbands = np.arange(34) + 1
+    for ant in range(13):
+        for pol in range(2):
+            if type(refcal) is dict:
+                refcal=[refcal]
+            for ref in refcal:
+                ind, = np.where(ref['flag'][ant, pol, :] == 0)
+                if unwrap:
+                    pha=np.unwrap(ref['pha'][ant, pol, ind])
+                    ax1[pol, ant].set_ylim([-20, 20])
+                else:
+                    pha=ref['pha'][ant, pol, ind]
+                    ax1[pol, ant].set_ylim([-4, 4])
+                ax1[pol, ant].plot(allbands[ind], pha, '.', markersize=5)
+                ax1[pol, ant].set_xlim([1, 34])
+                ax2[pol, ant].plot(allbands[ind], ref['amp'][ant, pol, ind], '.', markersize=5)
+                ax2[pol, ant].set_xlim([1, 34])
+                ax2[pol, ant].set_ylim([0, 1.])
+                if ant == 0:
+                    ax1[pol, ant].set_ylabel('Phase (radian)')
+                    ax2[pol, ant].set_ylabel('Amplitude')
+                else:
+                    ax1[pol, ant].set_yticks([])
+                    ax2[pol, ant].set_yticks([])
+                if pol == 1 and ant == 0:
+                    ax1[pol, ant].set_xlabel('Band #')
+                    ax2[pol, ant].set_xlabel('Band #')
+                if pol == 0:
+                    ax1[pol, ant].set_title('Ant ' + str(ant + 1))
+                    ax1[pol, ant].set_xticks([])
+                    ax2[pol, ant].set_title('Ant ' + str(ant + 1))
+                    ax2[pol, ant].set_xticks([])
 
 def sql2refcal(t):
     '''Supply a timestamp in Time format, return the closest refcal data'''
@@ -291,21 +354,26 @@ def sql2refcal(t):
     import stateframe as stf
     xml, buf = ch.read_cal(8, t=t)
     refcal = stf.extract(buf, xml['Refcal_Real']) + stf.extract(buf, xml['Refcal_Imag']) * 1j
+    flag = stf.extract(buf, xml['Refcal_Flag'])
     timestamp = Time(stf.extract(buf, xml['Timestamp']), format='lv')
     pha = np.angle(refcal)
     amp = np.absolute(refcal)
-    return {'pha': pha, 'amp': amp, 'timestamp': timestamp}
+    return {'pha': pha, 'amp': amp, 'flag':flag, 'timestamp': timestamp}
 
 def sql2refcalX(trange):
     import eovsapy.cal_header as ch
     import stateframe as stf
     xml, bufs = ch.read_calX(8,t=trange)
     refcals=[]
-    for buf in bufs:
-        ref = stf.extract(buf, xml['Refcal_Real']) + stf.extract(buf, xml['Refcal_Imag']) * 1j
-        timestamp = Time(stf.extract(buf, xml['Timestamp']), format='lv')
-        pha = np.angle(ref)
-        amp = np.absolute(ref)
-        refcals.append({'pha': pha, 'amp': amp, 'timestamp': timestamp})
+    for i,buf in enumerate(bufs):
+        try:
+            ref = stf.extract(buf, xml['Refcal_Real']) + stf.extract(buf, xml['Refcal_Imag']) * 1j
+            flag = stf.extract(buf, xml['Refcal_Flag'])
+            timestamp = Time(stf.extract(buf, xml['Timestamp']), format='lv')
+            pha = np.angle(ref)
+            amp = np.absolute(ref)
+            refcals.append({'pha': pha, 'amp': amp, 'flag':flag, 'timestamp': timestamp})
+        except:
+            continue
     return refcals
 
