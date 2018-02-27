9008a8bc8a34165a35179a8f7c366840ae07b9ce
sjyu1988 <sjyu1988@gmail.com>
2018-02-27 17:51:31 -0500

    update html_movie

--- html_movie.py
+++ html_movie.py
@@ -1,4 +1,4 @@
-def html_movie(t):
+def html_movie(t, dir=None, imgprefix='', htmlname=None):
     ''' After the quick-look images are created for a given date, this routine will
         write the movie.html file that allows them to be viewed as a movie.  Just
         call this with a Time() object containing the desired date.
@@ -6,29 +6,31 @@ def html_movie(t):
     import glob
     from util import Time
     datstr = t.iso[:10]
-    dir = '/common/webplots/qlookimg_10m/'+datstr.replace('-','/')+'/'
-    files = glob.glob(dir+'/*.png')
+    if not dir:
+        dir = '/common/webplots/qlookimg_10m/' + datstr.replace('-', '/') + '/'
+    files = glob.glob(dir + '/{}*.png'.format(imgprefix))
     files.sort()
     nfiles = len(files)
-    f = open('/common/webplots/qlookimg_10m/2017/08/21/eclipse_20170821.html','r')
+    f = open('/common/webplots/qlookimg_10m/2017/08/21/eclipse_20170821.html', 'r')
     lines = f.readlines()
     nlines = len(lines)
     f.close()
     skiplines = []
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         k = line.find('var imax')
         if k != -1:
-            lines[i] = line[:10]+'{:3d}'.format(nfiles)+line[13:]
+            lines[i] = line[:10] + '{:3d}'.format(nfiles) + line[13:]
         k = line.find('urls[')
-        if k != -1: 
+        if k != -1:
             skiplines.append(i)
     #print skiplines
-    f = open(dir+'/movie_'+datstr.replace('-','')+'.html','w')
-    for i in range(skiplines[1]-1):
+    if not htmlname:
+        htmlname = dir + '/movie_' + datstr.replace('-', '') + '.html'
+    f = open(htmlname, 'w')
+    for i in range(skiplines[1] - 1):
         f.write(lines[i])
     for i in range(nfiles):
-        f.write('urls[{:d}]=url_path+"'.format(i)+files[i][40:]+'";\n')
-    for i in range(skiplines[-1]+1,nlines):
+        f.write('urls[{:d}]=url_path+"'.format(i) + files[i][40:] + '";\n')
+    for i in range(skiplines[-1] + 1, nlines):
         f.write(lines[i])
     f.close()
-    
